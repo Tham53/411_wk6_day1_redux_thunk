5216cef2309e92650dd22c6de17eef86f54c0277
Tham53 <texas01988@gmail.com>
2020-10-31 17:37:18 -0500

    new

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
-import { createStore } from 'redux'
-import reducers from './reducers'
+import { createStore, applyMiddleware, } from 'redux'
 import state from './state'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
-export default createStore(reducers, state)
+export default createStore(reducers, state, applyMiddleware(thunk))
